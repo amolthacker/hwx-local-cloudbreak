28b388645f44ec42737e640fc5ed46ee7237db75
Amol Thacker <amolthacker@gmail.com>
2018-05-02 10:02:15 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# hwx-local-cloudbreak-deployer
-Run Hortonworks Cloudbreak Deployer locally
+# HWX Local Cloudbreak
+
+Run [Hortonworks Cloudbreak](https://docs.hortonworks.com/HDPDocuments/Cloudbreak/Cloudbreak-2.4.1/index.html) locally
+
+
+## Prerequisites:
+
+ * [VirtualBox](https://www.virtualbox.org/)
+ * [Vagrant](https://www.vagrantup.com/) with [hostmanager](https://github.com/devopsgroup-io/vagrant-hostmanager) plugin
+
+
+ ## Steps
+
+  1. Clone the repo
+     ```
+     $ git clone https://github.com/amolthacker/hwx-local-cloudbreak.git
+     ```
+
+  2. Modify the configuration and properties, as needed, in `vagrant/cbd.yml` 
+
+  3. Spin up the VM
+     ```
+     $ cd hwx-local-cloudbreak/vagrant
+
+     $ vagrant up
+
+     $ vagrant ssh hwx-cloudbreak
+     ```
+
+  4. Log in
+     ```
+     https://hwx-cloudbreak/sl/
+     ```
+
+## Operations
+
+  1. Start
+     ```
+     $ cd vagrant
+     $ vagrant up
+     $ vagrant ssh hwx-cloudbreak
+
+     $ hwx-cloudbreak $ cbd start
+     ```
+
+  2. Stop
+     ```
+     $ cd vagrant
+     $ vagrant ssh hwx-cloudbreak
+
+     hwx-cloudbreak $ sudo cbd kill
+     hwx-cloudbreak $ exit
+
+     $ vagrant halt
+     ```
---
+++ scripts/provision-cbd.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+###################
+# ANSI COLORS
+###################
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+GRAY='\033[0;37m'
+NC='\033[0m'
+###################
+
+###################
+# Functions
+###################
+
+function datetime {
+    echo `date +%m-%d-%yT%H:%M:%S`
+}
+
+function datetimefmtd {
+    echo $(datetime) | sed 's/[-:]/_/g'
+}
+
+PROG="cbd-provisioner"
+
+function logInfo {
+    echo -e "$(datetime) ${YELLOW}${PROG}${NC} [${GREEN}INFO${NC}] $@"
+}
+
+function logErr {
+    echo -e "$(datetime) ${YELLOW}${PROG}${NC} [${RED}ERROR${NC}] $@"
+}
+
+function exitWithErr {
+    if [ $# -gt 0 ]
+    then
+        logErr $@
+    fi
+    exit 1
+}
+
+function exitOnErr {
+    if [ $? -ne 0 ]
+    then
+        exitWithErr $@
+    fi
+}
+
+###################
+
+DOCKER_ENGINE_VERSION='1.9.1'
+DOCKER_REPO_BASE_URL='https://yum.dockerproject.org/repo/main/centos/7'
+DOCKER_REPO_GPG_URL='https://yum.dockerproject.org/gpg'
+
+CLOUDBREAK_VERSION=$1
+CLOUDBREAK_REPO_BASE_PATH='s3.amazonaws.com/public-repo-1.hortonworks.com/HDP/cloudbreak'
+CLOUDBREAK_DEPLOYER_INSTALL_DIR=$2
+CLOUDBREAK_DEPLOYER_PROFILE_SECRET=$3
+CLOUDBREAK_DEPLOYER_PROFILE_USER_EMAIL=$4
+CLOUDBREAK_DEPLOYER_PROFILE_USER_PASSWORD=$5
+CLOUDBREAK_DEPLOYER_IP=$6
+
+###################
+
+sudo -i
+
+logInfo "Installing/Updating requisite system packages ..."
+logInfo "-------------------------------------------------"
+yum -y update && yum -y install net-tools ntp wget lsof unzip tar iptables-services
+
+logInfo "Installing and Configuring iptables-services ..."
+logInfo "-------------------------------------------------"
+yum -y install iptables-services net-tools
+iptables --flush INPUT && iptables --flush FORWARD && service iptables save
+
+logInfo "Configuring Docker Repo ..."
+logInfo "-------------------------------------------------"
+cat > /etc/yum.repos.d/docker.repo <<EOF
+[dockerrepo]
+name=Docker Repository
+baseurl=$DOCKER_REPO_BASE_URL
+enabled=1
+gpgcheck=1
+gpgkey=$DOCKER_REPO_GPG_URL
+EOF
+
+logInfo "Installing & Starting Docker ..."
+logInfo "-------------------------------------------------"
+yum install -y "docker-engine-${DOCKER_ENGINE_VERSION}" "docker-engine-selinux-${DOCKER_ENGINE_VERSION}"
+systemctl start docker
+systemctl enable docker
+
+logInfo "Fetching & Configuring Cloudbreak Deployer ..."
+logInfo "-------------------------------------------------"
+yum -y install unzip tar
+curl -Ls ${CLOUDBREAK_REPO_BASE_PATH}/cloudbreak-deployer_${CLOUDBREAK_VERSION}_$(uname)_x86_64.tgz | sudo tar -xz -C /bin cbd
+
+mkdir -p ${CLOUDBREAK_DEPLOYER_INSTALL_DIR} && cd ${CLOUDBREAK_DEPLOYER_INSTALL_DIR}
+cat > Profile <<EOF
+export UAA_DEFAULT_SECRET="$CLOUDBREAK_DEPLOYER_PROFILE_SECRET"
+export UAA_DEFAULT_USER_EMAIL="$CLOUDBREAK_DEPLOYER_PROFILE_USER_EMAIL"
+export UAA_DEFAULT_USER_PW="$CLOUDBREAK_DEPLOYER_PROFILE_USER_PASSWORD"
+export PUBLIC_IP="$CLOUDBREAK_DEPLOYER_IP"
+EOF
+
+logInfo "Installing & Starting Cloudbreak ..."
+logInfo "-------------------------------------------------"
+logInfo ""
+logInfo "Generating docker-compose and uaa configs ..."
+cbd generate
+logInfo "Pulling and verifying images for Cloudbreak services ..."
+cbd pull
+logInfo "Starting Cloudbreak ..."
+cbd start
+logInfo "-------------------------------------------------"
+
+###################
